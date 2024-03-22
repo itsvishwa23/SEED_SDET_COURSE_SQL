6acc42f91a96a6b51b4b8287f941321a8124c462
itsvishwa23 <itsvishwa23@gmail.com>
2024-03-22 19:25:51 +0530

    DAY 4

---
+++ DAY_4/LAB_EXERCISE_5.sql
@@ -0,0 +1,7 @@
+-- desc employees;
+
+-- select EMPLOYEE_ID,LAST_NAME,SALARY,SALARY*16 as TOTAL_SALARY 
+-- from employees;
+
+-- select EMPLOYEE_ID,LAST_NAME,SALARY,SALARY*16 as TOTAL_SALARY,(SALARY*16-SALARY) as NEW_SALARY
+-- from employees;
