f3933cab448bb1122c45f62baa0389191c834260
itsvishwa23 <itsvishwa23@gmail.com>
2024-03-23 18:04:34 +0530

    Day 5

---
+++ DAY_5/CHARACTER_FUNCTION.sql
@@ -0,0 +1,8 @@
+-- select UPPER(first_name) from employees;
+-- select lower(first_name) from employees;
+-- select initcap(first_name)from employees;
+
+-- select concat(first_name,last_name) from employees;
+
+-- select round(2233.3564) from dual;
+-- select mod(2233.3564,5) from dual;
